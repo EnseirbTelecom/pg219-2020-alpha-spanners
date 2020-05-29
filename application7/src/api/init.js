520c5bad69a8431941efa0e00e34d0f8ed36c218
Railore <sergent.adrien@gmail.com>
2020-05-29 02:25:32 +0200

    fixed pastposition, various fix and improvement added sorts on server response (list positions, list of friends ) added a last lastModified field on friend so it can properly fullfill its notification usage (and correspondant index) added a template7 helper dateFormat to convert OSI date to localtime

--- application7/src/api/init.js
+++ application7/src/api/init.js
@@ -10,12 +10,13 @@ const url = 'mongodb://localhost:27017';
         .then(database => database.db("friendFinder"))
         .catch(err => { throw err });
 
-    const p1 = database.collection('users'   ).createIndex( { pseudo : 1 },                         { unique: true });
-    const p2 = database.collection('users'   ).createIndex( { mail : 1 },                           { unique: true });
-    const p3 = database.collection('positions').createIndex( { userId : 1 })                        
-    const p4 = database.collection('positions').createIndex( { arrivalTime : -1, departureTime:-1 });
-    const p5 = database.collection('friends').createIndex( { senderId : 1, receiverId:1 },          { unique: true });
-    Promise.all([p1, p2, p3, p4, p5])
+    const p1 = database.collection('users'    ).createIndex( { pseudo : 1 },                         { unique: true } );
+    const p2 = database.collection('users'    ).createIndex( { mail : 1 },                           { unique: true } );
+    const p3 = database.collection('positions').createIndex( { userId : 1 } );                  
+    const p4 = database.collection('positions').createIndex( { arrivalTime : -1, departureTime:-1 } );
+    const p5 = database.collection('friends'  ).createIndex( { senderId : 1, receiverId:1 },         { unique: true } );
+    const p6 = database.collection('friends'  ).createIndex( { lastModified : -1 } );
+    Promise.all([p1, p2, p3, p4, p5,p6])
         .then(res => console.log("indexes created"))
         .catch( err => {throw err} )
         .finally(ex => process.exit(0));
